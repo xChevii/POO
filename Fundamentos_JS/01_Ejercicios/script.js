31eda424e94e7bfe1d8ad4949a0e6f92c2053273
xChevii <luisvalero@liceovvh.cl>
2024-03-15 12:10:58 -0300

    ok

--- Fundamentos_JS/01_Ejercicios/script.js
+++ Fundamentos_JS/01_Ejercicios/script.js
@@ -2,15 +2,16 @@ var global = "Global";
 let local = "local";
 const constante = "Constante";
 
-console.log("variable: Global: " + global + "-local: "+ local + "-Constante: " + constante)
+console.log("variable: Global: " + global + "- local: "+ local + "- Constante: " + constante);
 
 const numeroDecimla = 5.5;
 
-let nuemero = 54;let numeroDecimal = 5.5;
+let nuemero = 54;
+let numeroDecimal = 5.5;
 let texto = "hola pendejos";
 let registroDeCompra = true;
-let Semmana = ["lunes", "martes", "miercoles", "jueves", "viernes"];
-let cosa = {anthony_david:3};
+let Semmana = ["lunes","martes","miercoles","jueves","viernes"];
+let cosa = {anthonydavid:3};
 let indefinida;
 let nulo = null;
 function suma() {return 4 + 4
@@ -18,3 +19,5 @@ function suma() {return 4 + 4
 
 console.log("Variable:\nEntero " + numero + "\nDecimal " + numeroDecimal + "\nTexto " + texto + "\nBoolean " + registroDeCompra + "\nDias " + Semmana +
 "\nObjeto " + cosa + "\nVariable indefinidas " + indefinida + "\nnull" + nulo + "\nresultado funcion " + suma);
+
+console.log( suma ());
