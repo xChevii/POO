32df5443b7d60b303614fc614134b2d74da7b356
xChevii <luisvalero@liceovvh.cl>
2024-03-15 11:08:46 -0300

    ok

--- Fundamentos_JS/01_Ejercicios/script.js
+++ Fundamentos_JS/01_Ejercicios/script.js
@@ -3,3 +3,15 @@ let local = "local";
 const constante = "Constante";
 
 console.log("variable: Global: " + global + "-local: "+ local + "-Constante: " + constante)
+
+const numeroDecimla = 5.5;
+
+let nuemero = 54;let numeroDecimla = 5.5;
+let texto "hola pendejos";
+let registroDeCompra = true;
+let Semmana = ["lunes", "martes", "miercoles", "jueves", "viernes"];
+let cosa = {anthony_david:3};
+let indefinida;
+let nulo = null;
+function suma() {return 4 + 4
+}
