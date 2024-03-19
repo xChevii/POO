56b93e4039dd806dfbae89524b9450aef02dfb75
xChevii <luisvalero@licevvh.cl>
2024-03-19 09:28:37 -0300

    ok

--- Fundamentos_JS/02_Ejercicios/script.js
+++ Fundamentos_JS/02_Ejercicios/script.js
@@ -1,9 +1,5 @@
 <script>
-// Ejercicio 1: Declaración de variables
-// Instrucciones: Declara dos variables numéricas llamadas 'actual' y 'nac'.
-// Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
-
-function declararVariables() {
+() {
 let actual = 2023;
 let nac = 2004;
 let resultado = actual - nac;
@@ -11,9 +7,7 @@ alert("Mi edad es: " + resultado);
 // Tu código aquí
 }
 
-// Ejercicio 2: Asignación de valores
-// Instrucciones: Completa la función para asignar el valor de 'x' a 'y' y luego muestra el valor de 'y'.
-function asignarValores() {
+() {
 let x = 15;
 let y = x;
 alert("El valor de Y es: " + y);
@@ -21,11 +15,7 @@ alert("El valor de Y es: " + y);
 // Tu código aquí
 }
 
-// Ejercicio 3: Operaciones matemáticas
-// Instrucciones: Completa la función para realizar las siguientes operaciones:
-// Suma 'a' y 'b', resta 'b' de 'a', multiplica 'a' por 'b', y divide 'a' entre 'b'.
-// Muestra los resultados en un mensaje.
-function realizarOperaciones() {
+() {
 let b = 5;
 let a = 10;
 let suma = a + b;
@@ -38,10 +28,7 @@ alert("la suma es " + suma + "\n la resta es " + resta + "\n la multi es " + mul
 // Tu código aquí
 }
 
-// Ejercicio 4: Manipulación de cadenas
-// Instrucciones: Completa la función para construir un mensaje utilizando las variables 'nombre' y 'apellido'.
-// Luego, muestra la longitud del mensaje y el primer carácter en el mensaje.
-function manipularCadenas() {
+() {
 let nombre = "Juan";
 let apellido = "Pérez";
 let mensaje = "El se llama: " + nombre + " " + apellido;
