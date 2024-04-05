bb9513d20cb513e421f4887ecb3d8e95a6e0893c
xChevii <luisvalero@liceovvh.cl>
2024-04-05 11:00:47 -0300

    ok

---
+++ Fundamentos_JS/Ejercicios_Extras/opppss.js
@@ -0,0 +1,17 @@
+console.log("que fue")
+
+function temperaturasTarea() // para indentificar las cosas
+{
+    let resultado = ""
+    let temps = prompt("la temperatura de tu mama es: ")
+
+    if(temps <0){
+        resultado = `esta haciendo mucho frio estamos a: <h2>${temps}°</h2>`
+    } else if (temps > 0, temps <=25){
+        resultado = `estamos a una muy buena temperatura, estamos a: <h2>${temps}°</h2>`
+    } else if (temps > 25) {
+        resultado = `esta haciendo mucho calor estamos a: <h2>${temps}°</h2>`
+    }
+    document.getElementById('lista').innerHTML = resultado;
+}
+
