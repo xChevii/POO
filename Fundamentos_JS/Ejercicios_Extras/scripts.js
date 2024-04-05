dcb890c1853fd839e041f926c1e63f616ae0bad2
xChevii <luisvalero@liceovvh.cl>
2024-04-05 10:21:51 -0300

    ok

---
+++ Fundamentos_JS/Ejercicios_Extras/scripts.js
@@ -0,0 +1,8 @@
+var temperatura = 20; 
+if (temperatura < 0) {
+  console.log("esta haciendo mucho frio");
+} else if (temperatura >= 0 && temperatura <= 25) {
+  console.log("esta en una temperatura ambiente");
+} else {
+  console.log("esta haciendo mucho calor");
+}
