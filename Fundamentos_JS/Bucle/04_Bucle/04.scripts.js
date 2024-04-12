10074fc749fcee7793df828f771fa75c76da1bf2
xChevii <luisvalero@liceovvh.cl>
2024-04-12 10:10:47 -0400

    ok

--- Fundamentos_JS/Bucle/04_Bucle/04.scripts.js
+++ Fundamentos_JS/Bucle/04_Bucle/04.scripts.js
@@ -1,10 +1,10 @@
 function encontrarNumeroMayor() {
   let numeros = [];
   for (let i = 1; i <= 5; i++) {
-    let numero = parseInt(prompt("Ingresa el número " + i + ":"));
+    let numero = parseInt(prompt("escriba el numero " + i + ":"));
     numeros.push(numero);
 
   }
   let numeroMayor = Math.max(...numeros);
-  document.getElementById("resultado").innerHTML = "<p>El número mayor es:</p> <br><h2>" + numeroMayor + "</h2>";
+  document.getElementById("resultado").innerHTML = "<p>encontraste el numero es:</p> <br><h2>" + numeroMayor + "</h2>";
 }
