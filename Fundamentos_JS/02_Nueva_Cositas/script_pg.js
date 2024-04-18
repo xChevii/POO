8210ea4dd26149eef34fd2fa6eabcf67559bf9d2
xChevii <luisvalero@liceovvh.cl>
2024-04-18 14:28:36 -0400

    ok

---
+++ Fundamentos_JS/02_Nueva_Cositas/script_pg.js
@@ -0,0 +1,12 @@
+function noNegativo(arr){
+    for(let 1 = 0; i < arr.length; i++){
+        if(arr[1], < 0){
+            arr[i] = 0;
+        }
+        return arr;+
+    }
+};
+let array = [-2,-5,4,-6]
+alert(noNegativo(array));
+
+document.getElementById("resultado").innerHTML = "<p>Números crecientes de 5 en 5:</p> <br><h2>" + array + "</h2>";
